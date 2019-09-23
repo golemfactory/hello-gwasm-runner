8afda60d40c1f01fcc1ab144beacb99b07bfc319
Jakub Konka <jakub.konka@golem.network>
2019-09-23 10:05:39 +0200

    Remove calls to dbg!

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,15 @@ fn main() -> Fallible<()> {
             const NUM_SUBTASKS: usize = 10;
             let arr: Vec<u64> = (1..=100).collect();
             let chunks: Vec<_> = arr.chunks(NUM_SUBTASKS).map(|x| (Vec::from(x),)).collect();
-            dbg!(&chunks);
             chunks.into_iter()
         },
         move |task: Vec<u64>| {
             let sum = task.into_iter().sum();
-            dbg!(sum);
             (sum,)
         },
         move |_: &Vec<String>, results: Vec<(_, _)>| {
             let given: u64 = results.iter().map(|(_, (result,))| result).sum();
             let expected: u64 = (1..=100).sum();
-            dbg!(given);
-            dbg!(expected);
             assert_eq!(expected, given, "sums should be equal")
         },
     )
