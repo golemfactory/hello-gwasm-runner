1c481e1592204ef95d8804e2794c50e35e3dcf1d
Jakub Konka <jakub.konka@golem.network>
2019-09-25 21:51:21 +0200

    Sync with tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
-use failure::Fallible;
 use gwasm_api::{dispatcher, SplitContext};
 
-fn main() -> Fallible<()> {
-    dispatcher::run(
-        move |_: &mut dyn SplitContext| {
-            const NUM_SUBTASKS: usize = 10;
-            let arr: Vec<u64> = (1..=100).collect();
-            let tasks: Vec<_> = arr.chunks(NUM_SUBTASKS).map(|x| (Vec::from(x),)).collect();
-            tasks.into_iter()
-        },
-        move |task: Vec<u64>| {
-            let sum = task.into_iter().sum();
-            (sum,)
-        },
-        move |_: &Vec<String>, results: Vec<(_, _)>| {
-            let given: u64 = results.iter().map(|(_, (result,))| result).sum();
-            let expected: u64 = (1..=100).sum();
-            assert_eq!(expected, given, "sums should be equal")
-        },
-    )
+fn main() {
+    dispatcher::run(split, exec, merge).unwrap()
+}
+
+type Task = Vec<u64>;
+type TaskResult = u64;
+
+fn split(_ctx: &mut dyn SplitContext) -> Vec<(Task,)> {
+    const NUM_SUBTASKS: usize = 10;
+    let arr: Vec<u64> = (1..=100).collect();
+    let mut tasks: Vec<(Task,)> = Vec::new();
+    for chunk in arr.chunks(NUM_SUBTASKS) {
+        let task: Task = chunk.to_vec();
+        tasks.push((task,));
+    }
+    tasks
+}
+
+fn exec(task: Task) -> (TaskResult,) {
+    let task_result: u64 = task.into_iter().sum();
+    (task_result,)
+}
+
+fn merge(_args: &Vec<String>, results: Vec<((Task,), (TaskResult,))>) {
+    let task_results: Vec<TaskResult> = results.into_iter().map(|(_, (result,))| result).collect();
+    let final_sum: u64 = task_results.into_iter().sum();
+    let expected: u64 = (1..=100).sum();
+    assert_eq!(final_sum, expected, "the sums should be equal")
 }
